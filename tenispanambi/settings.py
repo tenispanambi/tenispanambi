4b62ee6f19baf2b4a4b2a9a33c2c894b15286c59
Marcio Estula <mpestula78.apl@gmail.com>
2026-05-11 19:20:30 -0300

    Corrigir CSRF Railway

--- tenispanambi/settings.py
+++ tenispanambi/settings.py
@@ -104,3 +104,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://tenispanambi-production.up.railway.app',
+]
