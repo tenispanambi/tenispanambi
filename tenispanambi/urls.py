5c884a488aa06804594b5709fae08d9c085b0477
Marcio Estula <mpestula78.apl@gmail.com>
2026-05-11 23:37:47 -0300

    Servir arquivos static

--- tenispanambi/urls.py
+++ tenispanambi/urls.py
@@ -27,3 +27,12 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
