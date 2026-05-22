c15df4375cf1e1c9843015447611a0245b9413a7
Marcio Estula <mpestula78.apl@gmail.com>
2026-05-22 20:05:52 -0300

    Corrigir settings Railway

--- tenispanambi/settings.py
+++ tenispanambi/settings.py
@@ -3,58 +3,48 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-tenispanambi'
+SECRET_KEY = 'django-insecure-troque-isso-depois'
 
 DEBUG = True
 
 ALLOWED_HOSTS = [
-
     '127.0.0.1',
     'localhost',
     'tenispanambi-production.up.railway.app',
+    '.up.railway.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://tenispanambi-production.up.railway.app',
+    'https://*.up.railway.app',
 ]
 
-
 INSTALLED_APPS = [
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-
     'django.contrib.sessions',
-
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'core',
 ]
 
-
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'django.middleware.common.CommonMiddleware',
 
     'django.middleware.csrf.CsrfViewMiddleware',
 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
     'django.contrib.messages.middleware.MessageMiddleware',
-
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'tenispanambi.urls'
-
 
 TEMPLATES = [
     {
@@ -68,63 +58,49 @@ TEMPLATES = [
 
         'OPTIONS': {
             'context_processors': [
-
                 'django.template.context_processors.debug',
-
                 'django.template.context_processors.request',
 
                 'django.contrib.auth.context_processors.auth',
-
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-
 WSGI_APPLICATION = 'tenispanambi.wsgi.application'
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-
 AUTH_PASSWORD_VALIDATORS = []
-
 
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
-
 USE_TZ = True
-
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 LOGIN_URL = '/login/'
-
-LOGIN_REDIRECT_URL = '/meu-painel/'
-
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
